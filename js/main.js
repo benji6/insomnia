878726fc999444c65fc9d44d6840efb1b6eaf462
benji6 <benhall22@hotmail.co.uk>
2014-12-14 16:50:52 +0000

    using browserify

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var THREE = require('three.js');
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 64;
