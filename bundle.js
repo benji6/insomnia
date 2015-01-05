4523acd0eeb2c6b97d3460b0f280358903f059c8
benji6 <benhall22@hotmail.co.uk>
2015-01-05 21:59:50 +0000

    experimenting with shaders

--- bundle.js
+++ bundle.js
@@ -19,14 +19,31 @@ module.exports = function() {
 },{"three":6}],2:[function(require,module,exports){
 var THREE = require('three');
 
-var material = new THREE.MeshPhongMaterial({
-  color: 0xCCEE00
-});
-
 var geometry = new THREE.SphereGeometry(8, 32, 32);
 
+var vertexShader = 'varying vec3 vNormal;' +
+  'void main() {' +
+    'vNormal = normal;' +
+    'gl_Position = projectionMatrix *' +
+      'modelViewMatrix *' +
+      'vec4(position,1.0);' +
+  '}';
+
+var fragmentShader = 'varying vec3 vNormal;' +
+  'void main() {' +
+    'vec3 light = vec3(0.5,0.2,1.0);' +
+    'light = normalize(light);' +
+    'float dProd = max(0.0, dot(vNormal, light));' +
+    'gl_FragColor = vec4(dProd, dProd, dProd, 1.0);' +
+  '}';
+
+var shaderMaterial = new THREE.ShaderMaterial({
+  vertexShader: vertexShader,
+  fragmentShader: fragmentShader
+});
+
 module.exports = function() {
-  return new THREE.Mesh(geometry, material);
+  return new THREE.Mesh(geometry, shaderMaterial);
 };
 
 },{"three":6}],3:[function(require,module,exports){
